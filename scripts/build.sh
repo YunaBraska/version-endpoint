5d80d1fc85138b7288635e824f05fa4a146c639e
Yuna Morgenstern <io@yuna.berlin>
2020-02-09 22:48:36 +0100

    deployment fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env
-mvn clean verify -DskipTests=true deployment:run -Dbuilder -Dtag -Dupdate.major
+mvn clean verify -DskipTests=true -Dbuilder -Dtag -Dupdate.major
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env
 sh "$(dirname "$0")/build.sh"
-mvn package deploy -P release -DskipTests
+mvn clean package deploy -P release -DskipTests
